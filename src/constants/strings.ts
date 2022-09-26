873c2da207f0bdd2143e8edfbb3226e917a650f0
Josh Davies <josh@raileasy.co.uk>
2022-09-26 20:49:42 +0100

    :sparkles: Add settings modal

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,6 +2,7 @@ export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
 export const WIN_MESSAGES = ['You beat Durham! Congratulations!', 'Mark would be proud!', '3/3 marks from the judges!', 'Gary really enjoyed that!', 'SEAWEED!', 'And recalled to the next round: you!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = 'Statistics'
@@ -9,4 +10,4 @@ export const NEW_WORD_TEXT = 'New word in'
 export const SHARE_TEXT = 'Share'
 export const SUCCESS_RATE_TEXT = 'Success rate'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. Try using your device's default browser instead."
+    "You are using an embedded browser and may experience problems sharing or saving your results. Try using your device's default browser instead."
