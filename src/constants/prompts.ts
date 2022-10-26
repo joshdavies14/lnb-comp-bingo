065ccc58fee0b8e89008ab217cb60fb48b8d15be
Josh Davies <josh@raileasy.co.uk>
2022-10-26 21:53:35 +0100

    :speech_balloon: Add more prompts

--- src/constants/prompts.ts
+++ src/constants/prompts.ts
@@ -43,6 +43,8 @@ export const PROMPTS_LIST = [
   'Warwick team final in Team Match',
   'The Macarena comes on',
   'Lottie forgets the camera',
+  'Charlotte gets asked for safety pins',
+  'Someone attempts the alternative version of the Macarena',
 ]
 
 export const PROMPTS = (): string[] => shuffle(PROMPTS_LIST).slice(0, 25)
