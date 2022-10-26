2ea275d2a9a08c9bebdd70c8fe1720233249e631
Josh Davies <josh@raileasy.co.uk>
2022-10-26 22:24:23 +0100

    :speech_balloon: Remove unneeded strings

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -10,11 +10,5 @@ export const WIN_MESSAGES = [
 ]
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const SUCCESS_RATE_TEXT = 'Success rate'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
   "You are using an embedded browser and may experience problems sharing or saving your results. Try using your device's default browser instead."
