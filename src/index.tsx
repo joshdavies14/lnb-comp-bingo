092de4c3d67eafc1d10aac2ef1ec9232763fbcd7
Josh Davies <josh@joshdavies.tech>
2023-04-07 23:43:39 +0100

    :sparkles: Add Sentry monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import * as Sentry from "@sentry/react"
 import reportWebVitals from './reportWebVitals'
 import { AlertProvider } from './context/AlertContext'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.render(
   <React.StrictMode>
